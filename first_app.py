c11ea0a9be79573d9e913f9a7df2950f6c58ff8b
Etienne Lardeur <etienne.lardeur@gmail.com>
2020-11-24 14:30:56 +0100

    addition of desc.csv
    add desc.csv

--- first_app.py
+++ first_app.py
@@ -2,44 +2,66 @@ import streamlit as st
 import pandas as pd
 import os
 
-DATA_URL = 'https://raw.githubusercontent.com/EtienneLardeur/Streamlit_App/main/'
-DATA_PATH = os.path.join(DATA_URL, 'tiny.csv')
+REMOTE_URL = 'https://raw.githubusercontent.com/EtienneLardeur/Streamlit_App/main/'
+DATA_FILE_PATH = os.path.join(REMOTE_URL, 'tiny.csv')
+DESC_FILE_PATH = os.path.join(REMOTE_URL, 'desc.csv')
+# MDL_FILE_PATH = os.path.join(REMOTE_URL, 'model.sav')
+
+tiny = pd.read_csv(DATA_FILE_PATH)
+desc = pd.read_csv(DESC_FILE_PATH)
+
+# prepare lists
+Client_ID_list = tiny['SK_ID_CURR'].tolist()
+Field_list = desc['Row]'].tolist()
 
 st.write("""
-# Client application : credit scoring
+# Credit scoring of client's applications
 """)
 st.subheader('Overview - edition mode')
-tiny = pd.read_csv(DATA_PATH)
+
 
 st.write(tiny)
 
-Client_ID_list=tiny['SK_ID_CURR'].tolist()
+
 
 st.sidebar.header('User Input Values')
 
+# get (tbc adjustable features) of a given client by ID
 
-
-def user_input_features():
+def client_input_features():
 
 
     Client_ID = st.sidebar.selectbox('Please select Client ID', Client_ID_list, 0)
-
-    	##st.sidebar.add_rows
 
     data = {'Client_ID': Client_ID}
     
     features = pd.DataFrame(data, index=[0])
     return features
 
-df = user_input_features()
+df_client_id = client_input_features()
+
+# get description of a field
+
+st.sidebar.header('Get full description of a field')
+
+def field_description():
+
+
+    Field = st.sidebar.selectbox('Please select a field', Field_list, 0)
+
+    Description = desc[desc['Row'] == Field]['Description']
+    
+    return Description
+
+txt_field_desc = field_description()
 
 st.subheader('Selected Client ID')
 
-st.write(df)
+st.write(txt_field_desc)
 
 
 # Display prediction
-def predict(df):
+def predict(df_client_id):
 
     Risk_Flag = tiny['RISK_FLAG'][tiny['SK_ID_CURR'] == df['Client_ID']]
     return Risk_Flag
